08d21a2708a5363a97342a91fd82b864417e77cc
Jackson Jackson <kodedjackson@gmail.com>
2022-09-13 22:29:23 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors:{
+      'primary-dark': '#39393A',
+      'primary-light': '#F8FAE3',
+      'dark-blue': '#133C55',
+      'ligh-blue': '#40BCD8',
+      'yellow': 'F39237'
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+  },
+  plugins: [],
+}
